42b820f8ca9aea590eafbe26fe4ef4dc4a30e232
Armin <armin.bagheri.1380@gmail.com>
2016-05-26 12:56:19 +0430

    Add files via upload

---
+++ plugins/Me.lua
@@ -0,0 +1,12 @@
+do
+function run(msg, matches)
+local text = " FirstName:  "..(msg.from.first_name or '').."\nLastName:  "..(msg.from.last_name or '').."\nUserID:  "..msg.from.id.."\nUsername:  @"..(msg.from.username or '').."\nPhone:  +"..(msg.from.phone or '').."\n\nTotal Messages:  0  "
+return text
+end
+return {
+patterns = {
+"^[/!#][Mm][Ee]$",
+},
+run = run
+}
+end
